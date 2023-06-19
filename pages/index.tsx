426ccf962d3396e7ab422d32037369e11577face
Racheal Pennell <racheal.larimer@gmail.com>
2023-06-18 18:25:28 -0700

    make html form accessible and migrate into Next index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,124 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <Head>
+        <title>Personal Information Collector</title>
+      </Head>
+      <div className='flex flex-col'>
+        <form action='/submit' method='POST'>
+          <h1>Sensitive Data Collector</h1>
+          <p>
+            Required fields are followed by{' '}
+            <strong>
+              <span aria-label='required'>*</span>
+            </strong>
+          </p>
+          <section>
+            <h2>Personal Information</h2>
+            <p>
+              <label htmlFor='name'>
+                <span>Name</span>
+                <strong>
+                  <span aria-label='required'>*</span>
+                </strong>
+              </label>
+              <input
+                id='name'
+                type='text'
+                name='name'
+                placeholder='Legal Name'
+              />
+            </p>
+            <p>
+              <label htmlFor='email'>
+                <span>Email</span>{' '}
+                <strong>
+                  <span aria-label='required'>*</span>
+                </strong>
+              </label>
+              <input id='email' name='email' type='email' placeholder='Email' />
+            </p>
+            <p>
+              <label htmlFor='date-of-birth'>
+                <span>Date of Birth</span>{' '}
+                <strong>
+                  <span aria-label='required'>*</span>
+                </strong>
+              </label>
+              <input type='date' />
+            </p>
+            <p>
+              <label htmlFor='social-security-number'>
+                <span>Social Security Number</span>{' '}
+                <strong>
+                  <span aria-label='required'>*</span>
+                </strong>
+              </label>
+              <input type='string' placeholder='111-11-111' />
+            </p>
+          </section>
+          <section>
+            <h2>Financial Information</h2>
+            <p>
+              <label htmlFor='credit'>
+                <span>How much credit do you currently have available?</span>
+                <strong>
+                  <span aria-label='required'>*</span>
+                </strong>
+              </label>
+              <select name='credit' id='credit'>
+                <option value='low'>$0 - $5,000</option>
+                <option value='med'>$5,001 - $10,000</option>
+                <option value='high'>10,000+</option>
+              </select>
+            </p>
+            <fieldset>
+              <legend>Which assets do you currently have available?</legend>
+              <p>
+                <input type='checkbox' id='car' name='assets' value='car' />
+                <label htmlFor='car'>Car</label>
+              </p>
+              <p>
+                <input type='checkbox' id='house' name='assets' value='house' />
+                <label htmlFor='house'>House</label>
+              </p>
+              <p>
+                <input type='checkbox' id='boat' name='assets' value='boat' />
+                <label htmlFor='boat'>Boat</label>
+              </p>
+              <p>
+                <input
+                  type='checkbox'
+                  id='retirement-account'
+                  name='assets'
+                  value='retirement-account'
+                />
+                <label htmlFor='retirement-account'>Retirement Account</label>
+              </p>
+            </fieldset>
+          </section>
+          <p>
+            <label htmlFor='additional-comments'>Additional Comments</label>
+            <textarea
+              name='additional-comments'
+              id='additional-comments'
+              cols={30}
+              rows={10}
+            ></textarea>
+          </p>
+          <p>
+            <input type='checkbox' name='doesConsent' id='doesConsent' />
+            <label htmlFor='doesConsent'>
+              <span>I consent to storing my personal information</span>{' '}
+              <strong>
+                <span aria-label='required'>*</span>
+              </strong>
+            </label>
+          </p>
+        </form>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
